d91415a89810420e3aa96264a06713012751a29e
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-01-29 10:23:24 -0300

    project-done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import { FaGithub, FaReact } from "react-icons/fa";
 import { SiTypescript } from "react-icons/si";
 import {
@@ -7,34 +9,107 @@ import {
 } from "react-icons/tb";
 
 export default function Home() {
+  const [handleHover, setHandleHover] = useState("");
+
   return (
     <div className="w-full h-screen flex flex-col justify-center text-white bg-black items-center">
       <h1 className="mb-8 text-2xl">Precisão adaptativa</h1>
-      <div className="w-[36rem] h-[32rem] flex flex-col bg-[rgb(20,22,22)]">
+
+      <div className="w-[36rem] h-[32rem] flex flex-col hover:cursor-none bg-[rgb(20,22,22)]">
         <div className="w-[100%] h-[33.33%] flex">
-          <div className=" w-[100%] flex justify-center items-center">
-            <div className="p-4 rounded-full hover:bg-slate-500">
+          <div
+            onMouseEnter={() => setHandleHover("react")}
+            onMouseLeave={() => setHandleHover("")}
+            className="w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "react"
+                  ? "bg-[#999999] animate-in zoom-in-50 "
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
               <FaReact size={32} color="cyan" />
             </div>
           </div>
-          <div className=" w-[100%] flex justify-center items-center">
-            <TbBrandTailwind size={32} color="cyan" />
+          <div
+            onMouseEnter={() => setHandleHover("tailwind")}
+            onMouseLeave={() => setHandleHover("")}
+            className=" w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "tailwind"
+                  ? "bg-[#999999] animate-in zoom-in-50"
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
+              <TbBrandTailwind size={32} color="cyan" />
+            </div>
           </div>
         </div>
         <div className="w-[100%] h-[33.33%] flex">
-          <div className=" w-[100%] flex justify-center items-center">
-            <FaGithub size={32} color="cyan" />
+          <div
+            onMouseEnter={() => setHandleHover("git")}
+            onMouseLeave={() => setHandleHover("")}
+            className=" w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "git"
+                  ? "bg-[#999999]  animate-in zoom-in-50"
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
+              <FaGithub size={32} color="cyan" />
+            </div>
           </div>
-          <div className=" w-[100%] flex justify-center items-center">
-            <TbBrandJavascript size={32} color="cyan" />
+          <div
+            onMouseEnter={() => setHandleHover("js")}
+            onMouseLeave={() => setHandleHover("")}
+            className=" w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "js"
+                  ? "bg-[#999999]  animate-in zoom-in-50"
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
+              <TbBrandJavascript size={32} color="cyan" />
+            </div>
           </div>
         </div>
         <div className="w-[100%] h-[33.33%] flex">
-          <div className=" w-[100%] flex justify-center items-center">
-            <TbBrandNextjs size={32} color="cyan" />
+          <div
+            onMouseEnter={() => setHandleHover("next")}
+            onMouseLeave={() => setHandleHover("")}
+            className=" w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "next"
+                  ? "bg-[#999999]  animate-in zoom-in-50"
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
+              <TbBrandNextjs size={32} color="cyan" />
+            </div>
           </div>
-          <div className=" w-[100%] flex justify-center items-center">
-            <SiTypescript size={32} color="cyan" />
+          <div
+            onMouseEnter={() => setHandleHover("ts")}
+            onMouseLeave={() => setHandleHover("")}
+            className=" w-[100%] flex justify-center items-center"
+          >
+            <div
+              className={`${
+                handleHover === "ts"
+                  ? "bg-[#999999]  animate-in zoom-in-50"
+                  : null
+              } w-14 h-14 flex justify-center items-center rounded-full`}
+            >
+              <SiTypescript size={32} color="cyan" />
+            </div>
           </div>
         </div>
       </div>
